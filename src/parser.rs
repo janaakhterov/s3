9a3db5b11b2eb909e39a9da21f7a61e78a89a253
Daniel Akhterov <akhterovd@gmail.com>
2020-01-14 12:31:12 -0800

    Add Comments and Profiles to parser

--- src/parser.rs
+++ src/parser.rs
@@ -3,9 +3,13 @@ use nom::{
     bytes::complete::{
         tag,
         take,
+        take_till,
         take_while,
     },
-    character::complete::space0,
+    character::complete::{
+        newline,
+        space0,
+    },
     error::ErrorKind,
     number::streaming::be_u16,
     sequence::tuple,
@@ -16,33 +20,66 @@ use std::borrow::Cow;
 
 #[derive(Debug, PartialEq)]
 enum Token<'a> {
-    Key(Key),
+    Key(Key<'a>),
     Equals,
     Space,
     Value(&'a str),
+    Comment,
+    Profile(&'a str),
 }
 
 #[derive(Debug, PartialEq)]
-enum Key {
+enum Key<'a> {
     AwsAccessKeyId,
     AwsSecretAccessKey,
+    Unknown(&'a str),
 }
 
-named!(
-    key_value_tokens(&str) -> (Token<'_>, Token<'_>, Token<'_>, Token<'_>, Token<'_>),
-    tuple!(alt!(aws_access_key_id | aws_secret_access_key), space, equals, space, value)
-);
+fn key_value<'a>(input: &'a str) -> IResult<&'a str, (Token<'a>, Token<'a>)> {
+    let result = key(input)?;
+    let key = result.1;
 
-fn aws_access_key_id<'a>(input: &'a str) -> IResult<&'a str, Token<'a>> {
-    let tag = tag("aws_access_key_id")(input)?;
-    let tag = (tag.0, Token::Key(Key::AwsAccessKeyId));
-    Ok(tag)
+    let result = space0(result.0)?;
+    let result = equals(result.0)?;
+    let result = space0(result.0)?;
+
+    let result = value(result.0)?;
+    let value = result.1;
+
+    Ok((result.0, (key, value)))
 }
 
-fn aws_secret_access_key<'a>(input: &'a str) -> IResult<&'a str, Token> {
-    let tag = tag("aws_secret_access_key")(input)?;
-    let tag = (tag.0, Token::Key(Key::AwsSecretAccessKey));
-    Ok(tag)
+fn key<'a>(input: &'a str) -> IResult<&'a str, Token<'a>> {
+    let result = take_while(|ch: char| ch.is_lowercase() || ch == '_')(input)?;
+    let key = Token::Key(match result.1 {
+        "aws_secret_access_key" => Key::AwsSecretAccessKey,
+        "aws_access_key_id" => Key::AwsAccessKeyId,
+        _ => Key::Unknown(result.1),
+    });
+
+    Ok((result.0, key))
+}
+
+fn comment<'a>(input: &'a str) -> IResult<&'a str, Token> {
+    let tag = tag("#")(input)?;
+    let result = take_till(|ch: char| ch == '\n')(tag.0)?;
+
+    let result = if result.0.len() > 0 {
+        take(1usize)(result.0)?
+    } else {
+        result
+    };
+
+    let result = (result.0, Token::Comment);
+    Ok(result)
+}
+
+fn profile<'a>(input: &'a str) -> IResult<&'a str, Token> {
+    let result = tag("[")(input)?;
+    let result = take_while(|ch: char| ch.is_lowercase())(result.0)?;
+    let end = tag("]")(result.0)?;
+    let result = (end.0, Token::Profile(result.1));
+    Ok(result)
 }
 
 fn equals<'a>(input: &'a str) -> IResult<&'a str, Token<'a>> {
@@ -69,13 +106,13 @@ mod test {
     #[test]
     fn aws_access_key_id_test() {
         let token = Ok(("", Token::Key(Key::AwsAccessKeyId)));
-        assert_eq!(aws_access_key_id("aws_access_key_id"), token);
+        assert_eq!(key("aws_access_key_id"), token);
     }
 
     #[test]
     fn aws_secret_access_key_test() {
         let token = Ok(("", Token::Key(Key::AwsSecretAccessKey)));
-        assert_eq!(aws_secret_access_key("aws_secret_access_key"), token);
+        assert_eq!(key("aws_secret_access_key"), token);
     }
 
     #[test]
@@ -97,10 +134,9 @@ mod test {
             Token::Value("6KSUI28SEVTXB63GLSLU"),
         );
 
-        let (_, (key, _, _, _, value)) =
-            key_value_tokens("aws_access_key_id=6KSUI28SEVTXB63GLSLU")?;
+        let tokens = key_value("aws_access_key_id=6KSUI28SEVTXB63GLSLU")?.1;
 
-        assert_eq!((key, value), token);
+        assert_eq!(tokens, token);
 
         Ok(())
     }
@@ -112,12 +148,29 @@ mod test {
             Token::Value("NQMJwbNv0qjBBtAIPbV47JOnqrGCveuqVvO8XwuG"),
         );
 
-        let (_, (key, _, _, _, value)) = key_value_tokens(
-            "aws_secret_access_key   =    NQMJwbNv0qjBBtAIPbV47JOnqrGCveuqVvO8XwuG",
-        )?;
+        let tokens =
+            key_value("aws_secret_access_key   =    NQMJwbNv0qjBBtAIPbV47JOnqrGCveuqVvO8XwuG")?.1;
 
-        assert_eq!((key, value), token);
+        assert_eq!(tokens, token);
 
         Ok(())
+    }
+
+    #[test]
+    fn comment_test() {
+        let token = Ok(("", Token::Comment));
+        assert_eq!(comment("# adsflasdflkasdfjlakdjfkdsfj\n"), token);
+    }
+
+    #[test]
+    fn comment_no_newline_test() {
+        let token = Ok(("", Token::Comment));
+        assert_eq!(comment("# adsflasdflkasdfjlakdjfkdsfj"), token);
+    }
+
+    #[test]
+    fn profile_test() {
+        let token = Ok(("", Token::Profile("default")));
+        assert_eq!(profile("[default]"), token);
     }
 }
